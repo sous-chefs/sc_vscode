092765fc922e5bd7d5d5971b734f889a7526db76
Dan Webb <dan.webb@damacus.io>
2019-08-19 20:43:12 +0100

    Auto correct using cookstyle

--- test/fixtures/cookbooks/test/recipes/install.rb
+++ test/fixtures/cookbooks/test/recipes/install.rb
@@ -6,11 +6,11 @@ end
 
 # We need a couple prereqs for testing as servers do not have a GUI,
 # this is not expected in live, as no one would install vscode without a GUI?
-case node[:platform]
+case node['platform']
 when 'fedora'
   package_name = 'libX11-xcb'
 when 'ubuntu', 'debian'
-  package_name = ['libx11-xcb1', 'libxss1', 'libasound2', 'libxkbfile1']
+  package_name = %w(libx11-xcb1 libxss1 libasound2 libxkbfile1)
 end
 
 package 'testing-prereqs' do

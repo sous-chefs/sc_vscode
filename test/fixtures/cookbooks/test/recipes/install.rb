e2931befa997916fda176c23cf1faa8e95169d70
Dan Webb <dan.webb@damacus.io>
2022-05-17 15:19:20 +0100

    SQUASHME

--- test/fixtures/cookbooks/test/recipes/install.rb
+++ test/fixtures/cookbooks/test/recipes/install.rb
@@ -1,8 +1,7 @@
 # Make sure that Vagarant user is on the box for dokken
 include_recipe 'test::dokken'
 
-sc_vscode_installer 'install' do
-end
+sc_vscode_installer 'install'
 
 # We need a couple prereqs for testing as servers do not have a GUI,
 # this is not expected in live, as no one would install vscode without a GUI?
@@ -19,5 +18,5 @@ package 'testing-prereqs' do
 end
 
 sc_vscode_extension 'rebornix.ruby' do
-  user 'vagrant'
+  user node['user']
 end
